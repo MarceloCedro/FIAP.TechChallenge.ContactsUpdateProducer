fe51ce612c36298ab5ec163e90b50afceaa6507f
MarceloCedro <138161652+MarceloCedro@users.noreply.github.com>
2025-03-24 01:46:39 -0300

    feat: fix routes to api gateway

--- ContactsUpdateProducer.Integrations/ContactConsultManager.cs
+++ ContactsUpdateProducer.Integrations/ContactConsultManager.cs
@@ -17,7 +17,7 @@ namespace FIAP.TechChallenge.ContactsUpdateProducer.Integrations
         {
             try
             {
-                var url = $"{_configuration.GetSection("Integrations:ContactConsult")["BasePath"]}api/Contacts/{id}";
+                var url = $"{_configuration.GetSection("Integrations:ContactConsult")["BasePath"]}api/contacts/{id}";
 
                 HttpClient cliente = new HttpClient();
                 cliente.DefaultRequestHeaders.Add("Authorization", $"Bearer {token}");
@@ -42,7 +42,7 @@ namespace FIAP.TechChallenge.ContactsUpdateProducer.Integrations
         {
             try
             {
-                var url = _configuration.GetSection("Integrations:ContactConsult")["BasePath"]+"api/Token";
+                var url = _configuration.GetSection("Integrations:ContactConsult")["BasePath"]+"api/token";
                 var body = new CredentialDTO
                 {
                     Username = _configuration.GetSection("Credentials")["Username"],
